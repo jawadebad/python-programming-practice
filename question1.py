b6c1e70d349cca2e2139c8bd4cbc6e1dab94b82c
jawadebad <jawadebadd@gmail.com>
2026-09-06 18:48:37 +0100

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,20 @@
+## Jawad Ebad
+## Question 1: Basic List Creation & Access
+## 27/11/25
+
+def Question1():
+    # List of foods
+    foods = ["Chicken", "Rice", "Potato", "Steak", "Chips"]
+    
+    # Picking the 1st, 3rd and 5th items
+    first = [foods[0], foods[2], foods[4]]
+    print(first)
+    
+    # Print full list
+    print(foods)
+    
+    # Reverse the list
+    Reversed = foods[::-1]
+    print(Reversed)
+
+Question1()
